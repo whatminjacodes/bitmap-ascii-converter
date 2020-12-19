ae208b9aa85b6cc76c8bd317516f9cc5804a6e94
minjasilven <minja.silvennoinen@futurice.com>
2020-12-19 16:10:28 +0200

    Starting the project

---
+++ BitmapASCIIConverter/ImageConverter.cpp
@@ -0,0 +1,12 @@
+/*
+ *   IMAGE CONVERTER
+ * - Loads bitmap image file
+ * - Reads the (greyscale) pixel data and switches it to ASCII characters
+ * - Saves the image using new pixel data
+ */
+
+
+int main() {
+
+	return 0;
+}
